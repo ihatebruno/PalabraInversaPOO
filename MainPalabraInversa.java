5ff1db9a4e7d1f0fee76e84ab159d2b083edf8ba
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-05-09 16:54:37 -0600

    Palabra Inversa Comentado

--- MainPalabraInversa.java
+++ MainPalabraInversa.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class MainPalabraInversa {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        PalabraInversa plbr = new PalabraInversa();
+        Scanner sc = new Scanner(System.in); // Crear un objeto Scanner para leer la entrada del usuario.
+        PalabraInversa plbr = new PalabraInversa(); // Crear un objeto PalabraInversa.
 
+        // Solicitar al usuario que ingrese una palabra.
         System.out.println("Ingresa una palabra");
+        plbr.setPalabra(sc.nextLine()); // Leer la palabra ingresada por el usuario y establecerla en el objeto PalabraInversa.
 
-        plbr.setPalabra(sc.nextLine());
+        plbr.mostrarPalabraInversa(); // Mostrar la palabra invertida.
 
-        plbr.mostrarPalabraInversa(); 
-
-        System.out.println(plbr);
-
+        System.out.println(plbr); // Imprimir la representación en cadena del objeto PalabraInversa.
     }
 }
--- PalabraInversa.java
+++ PalabraInversa.java
@@ -1,35 +1,35 @@
 public class PalabraInversa {
+    private String palabra; // Variable para almacenar la palabra original.
+    private String palabraInversa; // Variable para almacenar la palabra invertida.
 
-    
-    private String palabra;
-    private String palabraInversa;
-    
-    
+    // Método para mostrar la palabra inversa.
     public void mostrarPalabraInversa() {
-        palabraInversa = obtenerPalabraInversa(); 
+        palabraInversa = obtenerPalabraInversa(); // Obtener la palabra inversa.
     }
     
+    // Método privado para obtener la palabra inversa.
     private String obtenerPalabraInversa() {
-        StringBuilder sb = new StringBuilder(palabra);
-        return sb.reverse().toString(); 
+        StringBuilder sb = new StringBuilder(palabra); // Crear un StringBuilder con la palabra original.
+        return sb.reverse().toString(); // Invertir la palabra y convertirla a String.
     }
 
+    // Métodos getter y setter para la palabra original.
     public String getPalabra() {
         return palabra;
     }
-    
     public void setPalabra(String palabra) {
         this.palabra = palabra;
     }
-    
-    
+
+    // Métodos getter y setter para la palabra inversa.
     public String getPalabraInversa() {
         return palabraInversa;
     }
-    
     public void setPalabraInversa(String palabraInversa) {
         this.palabraInversa = palabraInversa;
     }
+
+    // Método toString para representar el objeto PalabraInversa como una cadena de texto.
     @Override
     public String toString() {
         return "PalabraInversa [Palabra Original=" + palabra + ", Palabra Inversa=" + palabraInversa + "]";
